1616e744216aa9ceeb4722df072bcc818b484722
sigmaidetest <cehib@balanc3r.com>
2018-09-20 16:06:12 +0530

    aa

---
+++ SWTestBackend/lambda.js
@@ -0,0 +1,14 @@
+let AWS = require('aws-sdk');
+
+exports.handler = function (event, context, callback) {
+
+    console.log(event);
+    callback(null, {
+        "isBase64Encoded": true,
+        "statusCode": 200,
+        "headers": {
+            "headerName": "headerValue"
+        },
+        "body": JSON.stringify(event);
+    });
+}
